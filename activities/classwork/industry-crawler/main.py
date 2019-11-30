085a0f8b0750c710de1c3c4dc8a44c9b407eb218
anehik <karely_th@hotmail.com>
2019-11-29 19:36:45 -0600

    format file

--- activities/classwork/industry-crawler/main.py
+++ activities/classwork/industry-crawler/main.py
@@ -1,19 +1,19 @@
+
 import logging
 import json
 import fire
 
+
+
 from model import SIC
 from util import StringWrapper, pretty_print, timeit
-
 
 URL = "https://www.osha.gov/pls/imis/sic_manual.html"
 DEFAULT_INDUSTRY_FILE = "industries.json"
 
 logger = logging.getLogger(__name__)
 
-
 class Main(object):
-
     @staticmethod
     @timeit(logger)
     def download(filename=DEFAULT_INDUSTRY_FILE):
